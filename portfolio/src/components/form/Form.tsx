d3f5cd5e9edc0ec6a3e04e1393f63dc2ebf3cfc7
Dominck Nitecki <dominick.nitecki@outlook.com>
2022-02-25 22:23:36 -0600

    prettier

--- portfolio/src/components/form/Form.tsx
+++ portfolio/src/components/form/Form.tsx
@@ -1,45 +1,53 @@
-import "./Form.scss"
-import React from 'react'
+import "./Form.scss";
+import React from "react";
 import emailjs from "@emailjs/browser";
 
 export default function Contact() {
-
-
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('service_fgqcf6g', 'template_r2ej0ii', e.target, 'user_YGWBKQ0TxdOqra3kZl7jl')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_fgqcf6g",
+        "template_r2ej0ii",
+        e.target,
+        "user_YGWBKQ0TxdOqra3kZl7jl"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      e.target.reset()
-}
+        }
+      );
+    e.target.reset();
+  }
 
   return (
-  
     <div className="form">
-            <form  onSubmit={sendEmail}>
-                <div className="label">Name</div>
-                <div >
-                <input className="input" type="text" name="name" required />
-                </div>
-                <div className="label">Email</div>
-                <div >
-                <input className="input" type="email"  name="email" required />
-                </div>
-                <div className="label">Message</div>
-                <div >
-                <textarea className="text-area" name="message" required />
-                </div>
-                <button className="submit-button" type='submit' value='SendMessage'>
-                <img className='button-icon' src='./assets/mail-send-email.svg' alt='icon'/>
-                <div className="button-label"> Send Message </div>
-                
-                </button>
-            </form>
+      <form onSubmit={sendEmail}>
+        <div className="label">Name</div>
+        <div>
+          <input className="input" type="text" name="name" required />
         </div>
-        
-  )
+        <div className="label">Email</div>
+        <div>
+          <input className="input" type="email" name="email" required />
+        </div>
+        <div className="label">Message</div>
+        <div>
+          <textarea className="text-area" name="message" required />
+        </div>
+        <button className="submit-button" type="submit" value="SendMessage">
+          <img
+            className="button-icon"
+            src="./assets/mail-send-email.svg"
+            alt="icon"
+          />
+          <div className="button-label"> Send Message </div>
+        </button>
+      </form>
+    </div>
+  );
 }
--- portfolio/src/pages/projects/Projects.jsx
+++ portfolio/src/pages/projects/Projects.jsx
@@ -1,52 +1,55 @@
-import "./Projects.scss"
+import "./Projects.scss";
 import { motion } from "framer-motion";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-
-
 export default function Projects() {
-let history = useHistory();
-const pageVariants = {
+  let history = useHistory();
+  const pageVariants = {
     animate: {
-        opacity: 1,
-        y: 0,
+      opacity: 1,
+      y: 0,
     },
     initial: {
-        opacity: 0,
-        y: "100vh"
-
+      opacity: 0,
+      y: "100vh",
     },
     exit: {
-        opacity: 0,
-        y: "-100vh"
-    }
-};
-const pageTransitions = {
+      opacity: 0,
+      y: "-100vh",
+    },
+  };
+  const pageTransitions = {
     duration: 1.25,
     type: "tween",
     ease: "easeOut",
-    
   };
 
-const pageStyle = {
-    position: "absolute"
-}
-  return <motion.div
-  style={pageStyle}
-  initial="initial" 
-  animate="animate"
-  exit="exit"
-  variants={pageVariants}
-  transition={pageTransitions}>
+  const pageStyle = {
+    position: "absolute",
+  };
+  return (
+    <motion.div
+      style={pageStyle}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      variants={pageVariants}
+      transition={pageTransitions}
+    >
       <div className="wrapper-portfolio">
-        <div className="header-portfolio">
-        </div>
+        <div className="header-portfolio"></div>
         <div className="body-portfolio">
-            <div onClick={() => {history.push("/");}} className="title">Projects</div>
+          <div
+            onClick={() => {
+              history.push("/");
+            }}
+            className="title"
+          >
+            Projects
+          </div>
         </div>
-        <div className="footer">
-            
-        </div>   
+        <div className="footer"></div>
       </div>
-      </motion.div>;
+    </motion.div>
+  );
 }
